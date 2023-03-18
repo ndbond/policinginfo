e6690d55e6e46dd45615b149ab8fec8099e478ae
root <root@localhost.localdomain>
2023-03-18 15:57:53 -0700

    server, database, client up and running

---
+++ database/mongo-init.js
@@ -0,0 +1,2 @@
+db = db.getSiblingDB('policinginfo');
+db.createCollection('myCollection');
---
+++ server/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+mongoose.connect('mongodb://database:27017/mern-docker', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const connection = mongoose.connection;
+
+connection.once('open', () => {
+  console.log('MongoDB database connection established successfully');
+});
+
+app.listen(8000, () => {
+  console.log('Server is running on port 8000');
+});
+
